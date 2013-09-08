c228653a9088262b01e9929321c2aea13b3cf9fe
anubhav ashok <socter@cs.utexas.edu>
2013-09-08 14:45:54 -0500

    initial commit

---
+++ roauth2.r
@@ -0,0 +1,15 @@
+library(RCurl)
+library(rjson)
+oauth2_authentication<-function(consKey,consSec){
+cred_b64<-base64Encode(paste(consKey,":",consSec,sep=""))
+curl.opts<-curlOptions(httpheader = c(  Authorization = paste("Basic ",cred_b64,sep=""),
+				"Content-Type"="application/x-www-form-urlencoded;charset=UTF-8")
+			#verbose = TRUE
+		)
+
+bearerToken<-fromJSON(postForm(uri="https://api.twitter.com/oauth2/token",
+				grant_type="client_credentials",
+				.opts=curl.opts, 
+				style="POST"))	
+return (bearerToken)
+}
