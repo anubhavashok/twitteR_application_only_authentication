6a174d986248071ca1e9d87a9e12df25510483b1
anubhavashok <anubhavashok93@gmail.com>
2013-09-15 17:09:38 -0500

    added token invalidation

--- roauth2.r
+++ roauth2.r
@@ -27,21 +27,23 @@ bearerToken<-fromJSON(postForm(uri="https://api.twitter.com/oauth2/token",
 return (bearerToken)
 }
 
-#Refresh Token
-
-oauth2_invalidate_token<-function(consKey,consSec){
+#oauth2_invalidate_token: function to invalidate bearer token in case information is compromised
+#input: consumer Key, consumer Secret, bearer Token
+#output: boolean value indicating if token invalidation was successful
+oauth2_invalidate_token<-function(consKey,consSec, bearerToken){
 #encode consumer key and secret into base64("<consKey>:<consSec>")
 cred_b64<-base64Encode(paste(consKey,":",consSec,sep=""))
 #Set headers in curlOptions
 #Set verbose = TRUE for diagnostic information
-curl.opts<-curlOptions(httpheader = c(  Authorization = paste("Basic ",cred_b64,sep=""),
-				"Content-Type"="application/x-www-form-urlencoded;charset=UTF-8")
+curl.opts<-curlOptions(httpheader = c(  Authorization = paste("Basic ",cred_b64,sep=""))
 			#verbose = TRUE
 		)
-#Send twitter API a POST request to obtain a bearer token
-bearerToken<-fromJSON(postForm(uri="https://api.twitter.com/oauth2/invaltoken",
-				grant_type="client_credentials",
+#Send twitter API a POST request to invalidate bearerToken
+bearerToken2<-fromJSON(postForm(uri="https://api.twitter.com/oauth2/invalidate_token",
+				access_token=bearerToken,
 				.opts=curl.opts, 
 				style="POST"))	
-return (bearerToken)
+#Check if correct value returned
+if(bearerToken2==bearerToken) return (TRUE)
+return (FALSE)
 }
